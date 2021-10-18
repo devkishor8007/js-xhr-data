d6503f9178eeb7b57550267e930a3c915316e472
kishorkc <kishor8007kc@gmail.com>
2021-10-18 18:57:21 +0545

    initial commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("button").addEventListener("click", fetchTxt);
 function fetchTxt() {
   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "data.txt", true);
+  xhr.open("GET", "data.json", true);
 
   xhr.onprogress = function () {
     console.log("on progress state", this.readyState);
@@ -11,11 +11,10 @@ function fetchTxt() {
 
   xhr.onload = function () {
     console.log("on load readystate ", this.readyState);
-
     if (this.status === 200) {
-      document.getElementById(
-        "output"
-      ).innerHTML = `<p>${this.responseText}</p>`;
+      const data = JSON.parse(this.responseText);
+      const output = `<p>My name is ${data.name} and Learning ${data.learn}</p>`;
+      document.getElementById("output").innerHTML = output;
     }
   };
   xhr.send();
